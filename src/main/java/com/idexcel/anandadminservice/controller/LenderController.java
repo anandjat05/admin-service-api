18a85f3d81e297cd7dc0aa78949dc3aaa10cf560
anandjat05 <39242031+anandjat05@users.noreply.github.com>
2019-09-11 19:42:07 -0400

    Cross Origin Annotation added over the controller

--- src/main/java/com/idexcel/anandadminservice/controller/LenderController.java
+++ src/main/java/com/idexcel/anandadminservice/controller/LenderController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -26,6 +27,7 @@ import com.idexcel.anandadminservice.service.impl.AdminInterfaceService;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200")
 public class LenderController {
 	
 	private AdminInterfaceService adminInterfaceService;
